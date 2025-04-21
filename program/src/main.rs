246d7716d6310189a42f9041e74fa6a0525b2cb9
Lara <lara.rozman04@gmail.com>
2025-04-21 17:48:39 +0200

    Pojavi se ladja. Problem je, ker sva gledala ene stvari še za starejšo verzijo Bevy-a in so ene stvari precej drugačne zdej ...

--- program/src/main.rs
+++ program/src/main.rs
@@ -1,3 +1,50 @@
+use bevy::prelude::*;
+
+
 fn main() {
-    println!("Kaj je to bevy?!?!");
+    App::new()
+    .add_plugins(DefaultPlugins
+        // .set(WindowPlugin{
+        //     primary_window: Some(Window{
+        //         title: String::from("Space Rangers"),
+        //         position: WindowPosition::Centered(MonitorSelection::Primary),
+        //         resolution: Vec2::new(512., 512.).into(),
+        //         ..Default::default()
+        //     }),
+        //     ..Default::default()
+        // }))
+    )
+    .add_systems(Startup, (spawn_camera, spawn_player))
+    .run();
+}
+
+
+#[derive(Component)]
+struct Player {}
+
+fn spawn_camera(mut commands: Commands) {
+    commands
+      .spawn_empty()
+      .insert(Camera2d);
+  }
+
+fn spawn_player(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
+    commands.spawn(
+        (
+            Sprite{
+                image: asset_server.load("sprites/spaceShips_008.png"),
+                ..default()
+            },
+            // Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+            // SpriteBundle{
+            //     transform: Transform::from_xyz(window.width() / 2.0, window_height() / 2.0, 0.0),
+            //     sprite: asset_server.load("sprites/spaceShips_008.png"),
+            //     ..default()
+            // },
+            Player {},
+        )
+    );
 }
